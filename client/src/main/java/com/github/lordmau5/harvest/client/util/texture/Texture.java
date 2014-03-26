250dd537552b78bff62acc759ac793b1dc268ec3
Lordmau5 <dustin.hecken@web.de>
2014-03-26 20:31:15 +0100

    Namechange and Texture-Object

---
+++ client/src/main/java/com/github/lordmau5/harvest/client/util/texture/Texture.java
@@ -0,0 +1,16 @@
+package com.github.lordmau5.harvest.client.util.texture;
+
+/**
+ * Author: Lordmau5
+ * Date: 26.03.14
+ * Time: 20:27
+ */
+public class Texture {
+    String textureName;
+    boolean animated;
+
+    public Texture(String textureName, boolean animated) {
+        this.textureName = textureName;
+        this.animated = animated;
+    }
+}
