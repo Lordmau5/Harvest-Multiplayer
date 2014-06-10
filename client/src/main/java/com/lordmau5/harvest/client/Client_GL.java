062731a050324ed0d9de51c5a0266039bb9739e9
Lordmau5 <dustin.hecken@web.de>
2014-06-11 00:39:33 +0200

    Merge branch 'master' of K:\Development\Games\Harvest-Multiplayer with conflicts.

--- client/src/main/java/com/lordmau5/harvest/client/Client_GL.java
+++ client/src/main/java/com/lordmau5/harvest/client/Client_GL.java
@@ -1,4 +1,4 @@
-package com.lordmau5.harvest.client;
+package com.github.lordmau5.harvest.client;
 
 import org.lwjgl.LWJGLException;
 import org.lwjgl.Sys;
@@ -6,9 +6,11 @@ import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
+import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
 
+import java.awt.*;
 import java.io.IOException;
 
 /**
@@ -17,24 +19,20 @@ import java.io.IOException;
  * Time: 13:53
  */
 public class Client_GL {
-    float x = 400;
-    float y = 300;
+
+    float x = 400, y = 300;
     float rotation = 0;
     long lastFrame;
     int fps;
     long lastFPS;
 
     Texture texture;
-    float force = 1f;
-
-    public static void main(String args[]) {
-        Client_GL gl = new Client_GL();
-        gl.test();
-    }
+    TrueTypeFont testFont;
 
     void loadImages() {
         try {
             texture = TextureLoader.getTexture("PNG", org.newdawn.slick.util.ResourceLoader.getResourceAsStream("textures/objects/stone.png"));
+            testFont = new TrueTypeFont(new Font("Times New Roman", Font.BOLD, 24), false);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -65,52 +63,41 @@ public class Client_GL {
         Display.destroy();
     }
 
+    float force = 1f;
+
     public void update(int delta) {
         if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
-            if (Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)) {
+            if(Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)) {
                 rotation += 0.15f * delta * force;
                 force += 0.025f;
-                if (force > 15f) {
+                if(force > 15f)
                     force = 15f;
-                }
-            } else {
+            }
+            else {
                 x -= 0.35f * delta;
                 force = 1f;
             }
         }
-
         if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
-            if (Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)) {
+            if(Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)) {
                 rotation -= 0.15f * delta * force;
                 force += 0.025f;
-                if (force > 15f) {
+                if(force > 15f)
                     force = 15f;
-                }
-            } else {
+            }
+            else {
                 x += 0.35f * delta;
                 force = 1f;
             }
         }
 
-        if (Keyboard.isKeyDown(Keyboard.KEY_UP)) {
-            y -= 0.35f * delta;
-        }
-        if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
-            y += 0.35f * delta;
-        }
+        if (Keyboard.isKeyDown(Keyboard.KEY_UP)) y -= 0.35f * delta;
+        if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) y += 0.35f * delta;
 
-        if (x < 16) {
-            x = 16;
-        }
-        if (x > 800) {
-            x = 800;
-        }
-        if (y < 16) {
-            y = 16;
-        }
-        if (y > 600) {
-            y = 600;
-        }
+        if (x < 16) x = 16;
+        if (x > 800) x = 800;
+        if (y < 16) y = 16;
+        if (y > 600) y = 600;
 
         updateFPS(); // update FPS Counter
     }
@@ -133,16 +120,15 @@ public class Client_GL {
             fps = 0;
             lastFPS += 1000;
         }
-
         fps++;
     }
 
     public void initGL() {
         GL11.glEnable(GL11.GL_TEXTURE_2D);
-        GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        GL11.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-        GL11.glViewport(0, 0, 800, 600);
+        GL11.glViewport(0,0,800,600);
 
         GL11.glMatrixMode(GL11.GL_PROJECTION);
         GL11.glLoadIdentity();
@@ -156,25 +142,27 @@ public class Client_GL {
         //GL11.glColor3f(0.5f, 0.5f, 1.0f);
 
         texture.bind();
-        for (int tX = 0; tX < 2; tX++)
-            for (int tY = 0; tY < 2; tY++) {
+        GL11.glPushMatrix();
+        GL11.glTranslatef(x + texture.getImageWidth(), y + texture.getImageHeight(), 0);
+        //GL11.glRotatef(rotation, 0f, 0f, 1f);
+        GL11.glTranslatef(-x - texture.getImageWidth(), -y - texture.getImageHeight(), 0);
 
-                GL11.glPushMatrix();
-                GL11.glTranslatef(x + (tX * texture.getImageWidth()), y + (tY * texture.getImageHeight()), 0);
-                //GL11.glRotatef(rotation, 0f, 0f, 1f);
-                GL11.glTranslatef(-x - (tX * texture.getImageWidth()), -y - (tY * texture.getImageHeight()), 0);
-
-                GL11.glBegin(GL11.GL_QUADS);
-                GL11.glTexCoord2f(0, 0);
-                GL11.glVertex2f(x - texture.getImageWidth(), y - texture.getImageHeight());
-                GL11.glTexCoord2f(1, 0);
-                GL11.glVertex2f(x + texture.getImageWidth(), y - texture.getImageHeight());
-                GL11.glTexCoord2f(1, 1);
-                GL11.glVertex2f(x + texture.getImageWidth(), y + texture.getImageHeight());
-                GL11.glTexCoord2f(0, 1);
-                GL11.glVertex2f(x - texture.getImageWidth(), y + texture.getImageHeight());
-                GL11.glEnd();
-                GL11.glPopMatrix();
-            }
+        GL11.glBegin(GL11.GL_QUADS);
+            GL11.glTexCoord2f(0, 0);
+            GL11.glVertex2f(x - texture.getImageWidth(), y - texture.getImageHeight());
+            GL11.glTexCoord2f(1, 0);
+            GL11.glVertex2f(x + texture.getImageWidth(), y - texture.getImageHeight());
+            GL11.glTexCoord2f(1, 1);
+            GL11.glVertex2f(x + texture.getImageWidth(), y + texture.getImageHeight());
+            GL11.glTexCoord2f(0, 1);
+            GL11.glVertex2f(x - texture.getImageWidth(), y + texture.getImageHeight());
+        GL11.glEnd();
+        GL11.glPopMatrix();
     }
+
+    public static void main(String args[]) {
+        Client_GL gl = new Client_GL();
+        gl.test();
+    }
+
 }
