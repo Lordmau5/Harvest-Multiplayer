54d0a8f02dd2eafcee414ec4eabb464642a434e4
BinaryTENSHi <binarytenshi@gmail.com>
2014-12-22 20:35:07 +0100

    Gradle cleaning, lwjgl 3, netty 4.1 and main classes for client and server

---
+++ client/src/main/java/com/lordmau5/harvest/client/Client.java
@@ -0,0 +1,9 @@
+package com.lordmau5.harvest.client;
+
+/**
+ * Created by BinaryTENSHi on 22.12.2014.
+ */
+public class Client {
+    public static void main(String[] args) {
+    }
+}
---
+++ server/src/main/java/com/lordmau5/harvest/server/Server.java
@@ -0,0 +1,9 @@
+package com.lordmau5.harvest.server;
+
+/**
+ * Created by BinaryTENSHi on 22.12.2014.
+ */
+public class Server {
+    public static void main(String[] args) {
+    }
+}
